f9bd6647e982ed100cfd4c6c1d6a6db1cab8a118
alam1386 <alam1386@gmail.com>
2019-04-25 16:27:09 -0400

    Menu is created.

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { ApolloProvider, Query } from 'react-apollo'
 import gql from 'graphql-tag'
 import apolloClient from './apolloclient'
 import signUp from './components/Signup'
 import Index from './components/Index'
-
 import { BrowserRouter as Router, Route, Link} from 'react-router-dom'
+
+import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton'
+import MenuIcon from '@material-ui/icons/Menu';
+
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+import toRenderProps from 'recompose/toRenderProps';
+import withState from 'recompose/withState';
 
 const styles = {
   root: {
@@ -27,16 +33,50 @@ const styles = {
     marginRight: 20,
   },
 };
+const WithState = toRenderProps(withState('anchorEl', 'updateAnchorEl', null));
 
-function App() {
+function App(props) {
+  const { classes } = props;
   return(     
       <ApolloProvider client ={apolloClient}>
         <Router>
         <AppBar position="static">
-          <Toolbar>
-          <Typography variant="h6" color="inherit" >
-            <Link to="/signup" color="inherit" >To Signup </Link>
+        
+        <Toolbar>
+            <WithState>
+              {({ anchorEl, updateAnchorEl }) => {
+                const open = Boolean(anchorEl);
+                const handleClose = () => {
+                  updateAnchorEl(null);
+                };
+
+                return (
+                  <React.Fragment>
+                    <Button
+                    className={classes.menuButton} color="inherit" aria-label="Menu"
+                      aria-owns={open ? 'render-props-menu' : undefined}
+                      aria-haspopup="true"
+                      onClick={event => {
+                        updateAnchorEl(event.currentTarget);
+                      }}
+                    >
+                     <MenuIcon />
+                    </Button>
+                    <Menu id="render-props-menu" anchorEl={anchorEl} open={open} onClose={handleClose}>
+                      <MenuItem onClick={handleClose}>Dashbaord</MenuItem>
+                      <MenuItem onClick={handleClose}>Profile</MenuItem>
+                      <MenuItem onClick={handleClose}>My account</MenuItem>
+                      <MenuItem onClick={handleClose}>Logout</MenuItem>
+                    </Menu>
+                  </React.Fragment>
+                );
+              }}
+            </WithState>
+          <Typography variant="h6" color="inherit" className={classes.grow}>
+            Bazaar Market Place
           </Typography>
+          <Link to="/signup" color="inherit" >Signup </Link>
+          <Button to="/signup" color="inherit">Login</Button>
         </Toolbar>
         </AppBar>
           
@@ -47,5 +87,8 @@ function App() {
       </ApolloProvider>
   )   
 }
+signUp.propType = {
+  classes: PropTypes.object,
+};
 
-export default App;
+export default withStyles(styles)(App)
